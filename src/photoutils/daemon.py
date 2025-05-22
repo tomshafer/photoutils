66b29cbabd5424296bf102e659e42fceaccd077f
Tom Shafer <tom@tshafer.com>
2025-05-21 21:52:49 -0400

    Working, except for MOV files
    These work with `exiftool` itself, so I need to think about this more --
    try a few more things in notebooks to see what's happening. Really happy
    with all the other parts, though.

--- src/photoutils/daemon.py
+++ src/photoutils/daemon.py
@@ -2,73 +2,113 @@
 
 import logging
 import os
-from datetime import datetime
+import time
+from datetime import date, datetime
 from pathlib import Path
+from queue import Queue
+from threading import Thread
 
 from exiftool import ExifToolHelper
-
-# from PIL import ExifTags, Image, UnidentifiedImageError
 from watchdog.events import DirCreatedEvent, FileCreatedEvent, FileSystemEventHandler
 from watchdog.observers import Observer
 
+__all__ = ["watch_dir"]
+
 lg = logging.getLogger(__name__)
 
-type Exif = dict[str, str]
 
+def watch_dir(watched: Path) -> None:
+    """Watch a directory for images to file."""
+    if not watched.is_dir():
+        raise NotADirectoryError(watched)
 
-FILE_TYPES = {".RAF": "Raw Files", ".JPG": "JPEGs", ".MOV": "Videos"}
+    queue = Queue[Path | None]()
 
+    threads: list[Thread] = []
+    for i in range(os.process_cpu_count() or 1):
+        thread = Thread(target=run_thread, name=f"t-{i}", kwargs={"queue": queue})
+        thread.start()
+        threads.append(thread)
 
-# TODO: Use a queue in parallel
-class FileAdded(FileSystemEventHandler):
-    def __init__(self, root: Path) -> None:
-        self.root = root
-
-    def on_created(self, event: DirCreatedEvent | FileCreatedEvent) -> None:
-        # Ignore directories
-        if event.is_directory:
-            return
-
-        # Handle bytes-like inputs
-        if isinstance(event.src_path, (bytes, bytearray, memoryview)):
-            event.src_path = bytes(event.src_path).decode("utf-8")
-        path = Path(event.src_path)
-
-        # Read the file, create the structure, and move the file.
-        # TODO: Make this better, considering multiple exif tags
-        with ExifToolHelper() as et:
-            tags: list[Exif] = et.get_tags([event.src_path], "EXIF:DateTimeOriginal")  # type: ignore
-            date_str = tags[0]["EXIF:DateTimeOriginal"].strip().split()[0]
-            img_date = datetime.strptime(date_str, "%Y:%m:%d").date()
-
-        dest = self.root / str(img_date) / FILE_TYPES[path.suffix.upper()]
-        dest.mkdir(parents=True, exist_ok=True)
-        os.rename(path, dest / path.name)
-        os.chmod(dest / path.name, mode=0o644)
-
-        # Todo... parallelize/multithread
-        # Report/catch errors
-
-        # try:
-        #     with Image.open(path) as img:
-        #         exif = img.getexif()
-        #         print([ExifTags.TAGS[k] for k in exif])
-        # except UnidentifiedImageError:
-        #     lg.error(f"PIL could not read image: {path}")
-        #     return
-        # except FileNotFoundError:
-        #     lg.error(f"File not found: {path}")
-        #     return
-
-
-def watch_dir(target: Path) -> None:
     observer = Observer()
-    observer.schedule(FileAdded(target), str(target.absolute()), recursive=False)
+    observer.schedule(FileAddedHandler(queue), path=str(watched))
     observer.start()
 
     try:
-        while observer.is_alive():
-            observer.join(1)
-    finally:
+        while True:
+            time.sleep(1)
+    except (Exception, KeyboardInterrupt):
         observer.stop()
-        observer.join()
+        for _ in threads:
+            queue.put(None)
+
+    observer.join()
+    queue.join()
+    for thread in threads:
+        thread.join()
+
+
+class NotADirectoryError(OSError):
+    def __init__(self, p: Path) -> None:
+        super().__init__(f"the path {p} is not a directory")
+
+
+def run_thread(queue: Queue[Path | None]) -> None:
+    """Run a thread to pull and process events from a queue."""
+    while True:
+        path = queue.get()
+
+        # None is a sentinel for 'shut down'
+        if path is None:
+            queue.task_done()
+            return
+
+        lg.debug(f"Consuming path {path.name}")
+        if path.suffix.lower()[1:] in ("raf", "jpg", "dng"):
+            move_image(path, read_exif_date(path))
+            queue.task_done()
+
+
+class FileAddedHandler(FileSystemEventHandler):
+    """Put file-creation events onto a queue."""
+
+    def __init__(self, queue: Queue[Path | None]) -> None:
+        self.queue = queue
+
+    def on_created(self, event: DirCreatedEvent | FileCreatedEvent) -> None:
+        if isinstance(event, DirCreatedEvent):
+            return
+
+        file = src_path_to_path(event.src_path)
+        self.queue.put(file)
+        lg.debug(f"Putting path '{file.name}'")
+
+
+def move_image(file: Path, img_date: date) -> None:
+    """Move image-like files into a directory tree."""
+    DIRS = {"RAF": "Raw Files", "DNG": "Raw Files", "JPG": "JPEGs", "MOV": "Videos"}
+    dest = file.parent / str(img_date) / DIRS[file.suffix.upper()[1:]]
+    dest.mkdir(parents=True, exist_ok=True)
+
+    dest_file = dest / file.name
+    lg.debug(f"Moving {file.name} to {dest_file.relative_to(file.parent)}")
+    os.rename(file, dest_file)
+    os.chmod(dest_file, mode=0o644)
+
+
+def read_exif_date(file: Path) -> date:
+    """Read the file, create the structure, and move the file."""
+    # TODO: Make this better, considering multiple exif tags
+    lg.debug(f"Reading EXIF date for {file.name}")
+    with ExifToolHelper() as et:
+        tags: list[dict[str, str]] = et.get_tags([file], "EXIF:DateTimeOriginal")  # type: ignore
+        date_str = tags[0]["EXIF:DateTimeOriginal"].strip().split()[0]
+        img_date = datetime.strptime(date_str, "%Y:%m:%d").date()
+        lg.debug(f"EXIF date is {img_date}")
+        return img_date
+
+
+def src_path_to_path(src_path: bytes | str) -> Path:
+    """Cast an event src_path to pathlib.Path."""
+    src = src_path if isinstance(src_path, str) else bytes(src_path).decode()
+    return Path(src)
